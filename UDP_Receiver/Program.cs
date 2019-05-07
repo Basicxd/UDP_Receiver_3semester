631a7b4babb79df6bb9a1b62b0ebfbdf3c28842e
Mikail Fener <mika1292@edu.easj.dk>
2019-05-07 19:07:30 +0200

    Add project files.

---
+++ UDP_Receiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UDP_Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
