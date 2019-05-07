76150489ac93ae1b798a3e7bbf168b97898c1df8
Mikail Fener <mika1292@edu.easj.dk>
2019-05-07 20:30:18 +0200

    Working

--- UDP_Receiver/Program.cs
+++ UDP_Receiver/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace UDP_Receiver
 {
@@ -6,7 +10,33 @@ namespace UDP_Receiver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Skaber en UDPClient for det data der skal læses
+            UdpClient udpServer = new UdpClient(7000);
+
+            //Laver en IPEndPoint til at læse IP Address og Port nummer for afsenderen
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint remoteEndPoint = new IPEndPoint(ip, 7000);
+
+            try
+            {
+                Console.WriteLine("Receiver er startet");
+                while (true)
+                {
+                    Byte[] receivebBytes = udpServer.Receive(ref remoteEndPoint);
+
+                    string receivedData = Encoding.ASCII.GetString(receivebBytes);
+
+                    string[] data = receivedData.Split('\n');
+
+                    
+
+                    Console.WriteLine(receivedData);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
