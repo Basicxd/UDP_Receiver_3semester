f9437233bc31ad5db270296b2bfb1efe13c22cc6
basicxd <mikail-fener@hotmail.com>
2019-05-09 10:15:11 +0200

    Workinfdsldw

--- UDP_Receiver/Program.cs
+++ UDP_Receiver/Program.cs
@@ -1,24 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace UDP_Receiver
 {
     class Program
     {
+        public static string SensorUri = "https://localhost:44378/api/n%C3%B8dopkald";
+
+        public static async Task<HttpResponseMessage> AddCustomerAsync(Sensor sensor)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var jsonString = JsonConvert.SerializeObject(sensor);
+                Console.WriteLine("Data" + sensor);
+                StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(SensorUri, content);
+                if (response.StatusCode == HttpStatusCode.Conflict)
+                {
+                    throw new Exception("Customer already exists. Try another id");
+                }
+                response.EnsureSuccessStatusCode();
+
+                return response;
+            }
+
+        }
+
+        public static async Task<IList<Sensor>> GetCustomersAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(SensorUri);
+                IList<Sensor> cList = JsonConvert.DeserializeObject<IList<Sensor>>(content);
+                return cList;
+            }
+        }
+
         static void Main(string[] args)
         {
             // Skaber en UDPClient for det data der skal læses
+
             UdpClient udpServer = new UdpClient(7000);
 
             //Laver en IPEndPoint til at læse IP Address og Port nummer for afsenderen
             IPAddress ip = IPAddress.Any;
             IPEndPoint remoteEndPoint = new IPEndPoint(ip, 7000);
 
-            try
-            {
+            //try
+            //{
                 Console.WriteLine("Receiver er startet");
                 while (true)
                 {
@@ -28,15 +65,42 @@ namespace UDP_Receiver
 
                     string[] data = receivedData.Split('\n');
 
+                    string motion = data[0];
+                    string datoOgTid = data[1];
+
+                    string[] tidsSplit = datoOgTid.Split(' ');
+
+                    string dato = tidsSplit[1];
+                    string tid = tidsSplit[2];
+                
+                    //Console.WriteLine(motion);
+                    //Console.WriteLine(dato);
+                    //Console.WriteLine(tid);
+
+                    Sensor sensor = new Sensor();
+
                     
 
-                    Console.WriteLine(receivedData);
-                }
+                    HttpResponseMessage mi = AddCustomerAsync(new Sensor(sensor.Dato = dato, sensor.Tid = tid, sensor.Motion = motion)).Result;
+
+                    IList<Sensor> result = GetCustomersAsync().Result;
+                    foreach (Sensor C in result)
+                    {
+                        Console.WriteLine(C.ToString());
+                    }
+
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.ToString());
+            //
+            //}
+
+
+
+            
+
         }
     }
 }
---
+++ UDP_Receiver/Sensor.cs
@@ -0,0 +1,25 @@
+﻿namespace UDP_Receiver
+{
+    public class Sensor
+    {
+        public string Motion { get; set; }
+        public string Dato { get; set; }
+        public string Tid { get; set; }
+
+        public Sensor(string dato, string tid, string motion)
+        {
+            Motion = motion;
+            Dato = dato;
+            Tid = tid;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Dato)}: {Dato}, {nameof(Tid)}: {Tid}, {nameof(Motion)}: {Motion}";
+        }
+
+        public Sensor() { }
+
+
+    }
+}
